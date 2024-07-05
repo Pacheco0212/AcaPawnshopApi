0fe2433d8641a8150401f61a835cdbe765e7b168
Eduardo Pacheco <jpachecom1703@alumno.ipn.mx>
2024-07-04 21:48:42 -0600

    Last-Modification

--- src/main/java/com/acapawnshop/api/config/WebConfig.java
+++ src/main/java/com/acapawnshop/api/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://acapawnshop.vercel.app")
+                .allowedOrigins("http://localhost:3000/")
                 .allowedMethods("GET", "POST", "PUT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
